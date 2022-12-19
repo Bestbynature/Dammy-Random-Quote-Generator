83ccaa9044c4767846328161e12dc08609d86497
Dammy <bestbynature2010@gmail.com>
2022-12-19 05:55:40 +0100

    background colors changed with new quote

--- index.js
+++ index.js
@@ -358,7 +358,18 @@ let quoteBank = [
 ]
 
 
+const pickColor = () =>{
+  const colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c',
+  '#9b59b6', '#FB6964', '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857'];
 
+  randColorIndex = Math.floor(Math.random()*colors.length)
+  let nextColor = colors[randColorIndex]
+  document.getElementById("text").style.color = nextColor;
+  document.getElementById("author").style.color = nextColor;
+  document.getElementById("body").style.backgroundColor = nextColor;
+  document.getElementById("quote-box").style.color = nextColor;
+
+}
 
 
 const pickQuote = () =>{
@@ -392,6 +403,8 @@ if (!quoteBank[randIndex].author){
 
 
     document.getElementById("tweet-quote").href = tweetLink
+
+    pickColor();
 }
 
 
